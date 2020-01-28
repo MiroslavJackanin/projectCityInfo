72cc52bd995d4b6372f75ebfd3b3eda8dd8ca26e
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2020-01-28 10:50:44 +0100

    added class City

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,28 @@
 package sample;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
     public ComboBox<String> combo1;
     public ComboBox<String> combo2;
+    public Button okButton;
+    public Label population;
     List countries;
     List cities;
 
     public Controller() {
         Database db = new Database();
         countries = db.getCountries();
+    }
+    public void initialize(){
+        combo2.setDisable(true);
+        okButton.setDisable(true);
     }
 
     public void populateCB1(){
@@ -21,12 +32,36 @@ public class Controller {
     }
 
     public String getCB1Value(){
+        if (combo1.getValue()==null){
+            combo2.setDisable(true);
+        }else{
+            combo2.setDisable(false);
+        }
         return combo1.getValue();
+    }
+
+    public String getCB2Value(){
+        if (combo2.getValue()==null){
+            okButton.setDisable(true);
+        }else{
+            okButton.setDisable(false);
+        }
+        return combo2.getValue();
     }
 
     public void populateCB2(){
         Database db = new Database();
-        cities = db.getCities(getCB1Value());
-        combo2.getItems().setAll(cities);
+        List<City> city;
+        city = db.getCities(getCB1Value());
+        combo2.getItems().clear();
+        for(City s: city){
+            combo2.getItems().add(s.getName());
+        }
+    }
+
+    public void getInfo(){
+        Database db = new Database();
+        String pop = db.getPopulation(getCB1Value(), getCB2Value());
+        population.setText(pop);
     }
 }
--- src/sample/Database.java
+++ src/sample/Database.java
@@ -23,20 +23,43 @@ public class Database {
     }
 
     public List getCities(String country){
-        List<String> list = new ArrayList<>();
+        List<City> list = new ArrayList<>();
         try{
             Connection con = getConnection();
 
-            PreparedStatement ps = con.prepareStatement("SELECT city.name FROM country JOIN city ON country.code = city.countrycode WHERE country.name LIKE ? ");
+            PreparedStatement ps = con.prepareStatement("SELECT city.name, json_extract(info, '$.Population') AS Info, Code, Code2 FROM country JOIN city ON country.code = city.countrycode WHERE country.name LIKE ? ");
             ps.setString(1, country);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                list.add(rs.getString("Name"));
+                String name = rs.getString("city.name");
+                String code2 = rs.getString("Code2");
+                String code3 = rs.getString("Code");
+                int population = rs.getInt("Info");
+                City city = new City(name, population, code3, code2);
+                list.add(city);
             }
         }catch (Exception e){
             e.printStackTrace();
         }
         return list;
+    }
+
+    public String getPopulation(String country, String city){
+        String population = null;
+        try{
+            Connection con = getConnection();
+
+            PreparedStatement ps = con.prepareStatement("SELECT json_extract(info, '$.Population') FROM city JOIN country ON city.countryCode = country.code WHERE country.name LIKE ? AND city.name LIKE ?");
+            ps.setString(1, country);
+            ps.setString(2, city);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                population = rs.getString(1);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return population;
     }
 
     private Connection getConnection() throws ClassNotFoundException, SQLException {
